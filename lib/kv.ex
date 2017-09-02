446474802cccb3f4e620542cf5539a1d3ed7c06c
esensar <es.ensar@gmail.com>
2017-09-02 11:48:25 +0200

    Add supervisor for handling registry

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,7 +1,17 @@
 defmodule KV do
+  use Application
   @moduledoc """
   Documentation for KV.
+
+  Simple key-value storage based on named buckets
   """
+
+  @doc """
+  Starts the `KV.Supervisor' when application starts up
+  """
+  def start(_type, _args) do
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
 
   @doc """
   Hello world.
